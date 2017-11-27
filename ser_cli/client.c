239745bb3fe8c76d99bdc267f6dc3d1eefa34a57
Halo <halo@localhost.localdomain>
2017-11-27 04:27:26 -0500

    Modify Ser_Cli

--- ser_cli/client.c
+++ ser_cli/client.c
@@ -1,42 +1,44 @@
 #include"utili.h"
 
-int main()
+
+int main(int argc, char *argv[])
 {
 	int sockCli = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockCli == -1)
 	{
-		perror("sock error!\n");
+		perror("socket.");
 		exit(1);
 	}
 
 	struct sockaddr_in addrSer;
 	addrSer.sin_family = AF_INET;
-	addrSer.sin_port = htons(SERVER_PORT);
-	addrSer.sin_addr.s_addr = inet_addr(SERVER_IP);
+	addrSer.sin_port = htons(atoi(argv[2]));
+	addrSer.sin_addr.s_addr = inet_addr(argv[1]);
 
-	socklen_t len = sizeof(struct sockaddr);
-	int ret = connect(sockCli, (struct sockaddr*)&addrSer, len);
+	socklen_t addrlen = sizeof(struct sockaddr);
+	int ret = connect(sockCli,(struct sockaddr*)&addrSer, addrlen);
 	if(ret == -1)
 	{
-		perror("connet.");
+		printf("Client Connect Server Failed.\n");
+		exit(1);
 	}
 	else
-	{
-		printf("Client connet Server success!\n");
-	}
-	char sendbuf[BUFFER_SIZE];
-	char recvbuf[BUFFER_SIZE];
+	  printf("Client Connect Server Success.\n");
+
+	char sendbuf[256];
+	char recvbuf[256];
+
 	while(1)
 	{
-		recv(sockCli, recvbuf, BUFFER_SIZE, 0);
+		recv(sockCli,recvbuf, 256,0);
 		printf("Ser:> %s\n",recvbuf);
 		printf("Cli:> ");
-		scanf("%s", sendbuf);
-		if(!strcmp(sendbuf,"quit"))
-		  break;
-		send(sockCli, sendbuf, strlen(sendbuf)+1, 0);
+		scanf("%s",sendbuf);
+		send(sockCli,sendbuf,strlen(sendbuf)+1, 0);
 	}
-	
 	close(sockCli);
 	return 0;
 }
+
+
+
--- ser_cli/server.c
+++ ser_cli/server.c
@@ -1,73 +1,56 @@
 #include"utili.h"
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	int sockSer = socket(AF_INET,SOCK_STREAM, 0);
+	int sockSer = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockSer == -1)
 	{
-		perror("error!\n");
+		perror("socket.");
 		exit(1);
 	}
 
 	struct sockaddr_in addrSer, addrCli;
 	addrSer.sin_family = AF_INET;
-	addrSer.sin_port =  htons(SERVER_PORT);//host to network short convert
-	addrSer.sin_addr.s_addr = inet_addr("SERVER_IP");
+	addrSer.sin_port = htons(atoi(argv[2]));
+	addrSer.sin_addr.s_addr = inet_addr(argv[1]);
 
-	/*
 	int yes = 1;
-	setsockopt(sockSer, SO_REUSEADDR, &yes, sizeof(int));
-*/
-	socklen_t len = sizeof(struct sockaddr);
-	int ret = bind(sockSer, (struct sockaddr*)&addrCli, len);
-	if(!strcmp(sendbuf,"quit"))
-	   51           break;
-	 52         send(sockConn, sendbuf, strlen(sendbuf)+1, 0);
-	  53         recv(sockConn, recvbuf, BUFFER_SIZE, 0);
-	   54         printf("Cli:> %s\n",recvbuf);
-	    55     }
- 56     close(sockSer);
- 57     return 0;
-f(ret == -1)
-	{
-		perror("bind.\n");
-		exit(1);
-	}
+	setsockopt(sockSer, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-	ret = listen(sockSer, LISTEN_QUEUE_SIZE);
+	socklen_t addrlen =  sizeof(struct sockaddr);
+	int ret = bind(sockSer, (struct sockaddr*)&addrSer, addrlen);
 	if(ret == -1)
 	{
-		perror("listen.\n");
+		perror("bind.");
 		exit(1);
 	}
 
-	printf("Server wait Client Connect.....\n");
+	ret = listen(sockSer,5);
 
-	int sockConn = accept(sockSer, (struct sockaddr*)&addrCli,&len);
+	int sockConn = accept(sockSer, (struct sockaddr*)&addrCli, &addrlen);
 	if(sockConn == -1)
 	{
-		perror("Server accept Client error!\n");
+		printf("Server Accept Client Connect failed.\n");
 		exit(1);
 	}
 	else
-	{
-		printf("Server accept Client success!\n");
-		printf("Client IP:> %s\n",inet_ntoa(addrCli.sin_addr));
-		printf("Client Port:> %d\n",addrCli.sin_port);
-	}
-	char sendbuf[BUFFER_SIZE];
-	char recvbuf[BUFFER_SIZE];
+	  printf("Server Accept Client Success.\n");
+	
+	char sendbuf[256];
+	char recvbuf[256];
+
 	while(1)
 	{
 		printf("Ser:> ");
-		scanf("%s",sendbuf);
-		if(!strcmp(sendbuf,"quit"))
-		  break;
+		scanf("%s",&sendbuf);
 		send(sockConn, sendbuf, strlen(sendbuf)+1, 0);
-		recv(sockConn, recvbuf, BUFFER_SIZE, 0);
+		recv(sockConn, recvbuf, 256, 0);
 		printf("Cli:> %s\n",recvbuf);
 	}
 	close(sockSer);
 	return 0;
 }
+
+
+
